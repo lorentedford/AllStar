7b7e330f1974dd36d2c8a15a429f02d796046069
root <root@live-build.msgstor.com>
2015-03-13 13:52:27 -0400

    tweaks

--- scripts/build_asterisk.sh
+++ scripts/build_asterisk.sh
@@ -8,6 +8,9 @@
 
 cd /usr/src/astsrc-1.4.23-pre/asterisk/
 
+# patch for ulaw Core and Extras Sound Packages
+patch < /srv/patches/patch-asterisk-menuselect.makeopts
+
 # patch for SSL used in res_crypto
 patch < /srv/patches/patch-configure
 patch < /srv/patches/patch-configure.ac
