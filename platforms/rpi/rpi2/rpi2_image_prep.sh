e444a5b5b3779cf807cbf78916cbf0d4ad6d294d
root <root@live-build.msgstor.com>
2015-03-14 22:40:28 -0400

    add recreate ssh keys

--- platforms/rpi/rpi2/rpi2_image_prep.sh
+++ platforms/rpi/rpi2/rpi2_image_prep.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+#########################################################
+#                                                       #
+# Image prepare script was built for rpi2 install.      #
+#                                                       #
+#########################################################
 
 # change governor to performance
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -23,9 +28,11 @@ apt-get update -y
 apt-get upgrade -y
 
 curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update && chmod +x /usr/bin/rpi-update
-rpi-update
+# rpi-update
 
 # Add re-generate SSL keys <--------------------------------
+rm /etc/ssh/ssh_host_*
+dpkg-reconfigure openssh-server
 
 echo "If you saw no errors above REBOOT now!"
 
